Working Fresnel, still need to fix numerical error with RayleighSommerfeldProp

source/Optics.py
@@ -1,4 +1,5 @@
 import torch
+import torch.fft as fft
 import numpy as np
 from Wavefront import Wavefront
 
@@ -20,19 +21,121 @@ class OpticalElement:
         pass
 
 
-class FraunhoferProp(OpticalElement):
+class RayleighSommerfeldProp(OpticalElement):
+    """
+    Propagates the wavefront using the Rayleigh–Sommerfeld expression. We
+    fourier transform the field, multiply by the transfer function then
+    transform back. This is the most accurate propagation method.
+    """
 
-    def __init__(self, distance):
-        self.z = distance
+    def __init__(self, z):
+        """
+        :param z: Propagation distance.
+        """
+        self.z = z
 
-    def propagate(self, wavefront, pad=False):
+    def propagate(self, wavefront, pad=None):
+        """
+        Propogates wavefront.
+        :param wavefront: Wavefront to be propagated.
+        :param pad: Int setting padding amount. Default is none
+        """
 
         if pad:
-            wavefront.pad_wavefront()
+            wavefront.pad_wavefront(pad)
+
+        wave_k = wavefront.omega / c_light
+        lambd = 2.0 * torch.pi / wave_k
+        fx = fft.fftshift(fft.fftfreq(wavefront.n_samples_xy[0],
+                                      d=wavefront.delta[0],
+                                      device=wavefront.device))
+        fy = fft.fftshift(fft.fftfreq(wavefront.n_samples_xy[1],
+                                      d=wavefront.delta[1],
+                                      device=wavefront.device))
+        fx, fy = torch.meshgrid(fx, fy, indexing="ij")
+        tran_func = torch.exp(1j * 2. * torch.pi * self.z
+                              * (1. / lambd**2. - fx**2. - fy**2.)**0.5)
+        fig, ax = plt.subplots()
+        ax.pcolormesh(((1. - lambd**2 * fx**2. - lambd**2 *
+        fy**2.)**0.5).cpu())
+        plt.show()
+        field = wavefront.field.reshape(wavefront.n_samples_xy[0],
+                                        wavefront.n_samples_xy[1], 2)
+        field = fft.fftshift(fft.fft2(field, dim=(0, 1)), dim=(0, 1))
+        new_field = fft.ifft2(fft.ifftshift(field * tran_func[:, :, None],
+                                            dim=(0, 1)), dim=(0, 1))
+        wavefront.field = new_field.flatten(0, 1)
+        wavefront.z = wavefront.z + self.z
+
+
+class FresnelProp(OpticalElement):
+
+    def __init__(self, z):
+        """
+        :param z: Propagation distance.
+        """
+        self.z = z
+
+    def propagate(self, wavefront, pad=None):
+        """
+        Propogates wavefront.
+        :param wavefront: Wavefront to be propagated.
+        :param pad: Int setting padding amount. Default is none
+        """
+
+        if pad:
+            wavefront.pad_wavefront(pad)
+
+        wave_k = wavefront.omega / c_light
+        lambd = 2.0 * torch.pi / wave_k
+        fx = fft.fftshift(fft.fftfreq(wavefront.n_samples_xy[0],
+                                      d=wavefront.delta[0],
+                                      device=wavefront.device))
+        fy = fft.fftshift(fft.fftfreq(wavefront.n_samples_xy[1],
+                                      d=wavefront.delta[1],
+                                      device=wavefront.device))
+        fx, fy = torch.meshgrid(fx, fy, indexing="ij")
+
+        tran_func = torch.exp(-1j * torch.pi * lambd * self.z
+                              * (fx**2. + fy**2))
+        tran_func = torch.exp(torch.tensor(1j * wave_k * self.z)) * tran_func
+        field = wavefront.field.reshape(wavefront.n_samples_xy[0],
+                                        wavefront.n_samples_xy[1], 2)
+        field = fft.fftshift(fft.fft2(field, dim=(0, 1)), dim=(0, 1))
+        new_field = fft.ifft2(fft.ifftshift(field * tran_func[:, :, None],
+                                            dim=(0, 1)), dim=(0, 1))
+        wavefront.field = new_field.flatten(0, 1)
+        wavefront.z = wavefront.z + self.z
+
+
+class FraunhoferProp(OpticalElement):
+    """
+    Propagates the wavefront by some distance using the Fraunhofer method.
+    This can also be used to propagate to the fourier plane of a lens when
+    the Fresnel approximation holds.
+    """
+
+    def __init__(self, z):
+        """
+        :param z: Propagation distance (focal length is propagating to lens
+                  focus)
+        """
+        self.z = z
+
+    def propagate(self, wavefront, pad=None):
+        """
+        Propogates wavefront.
+        :param wavefront: Wavefront to be propagated.
+        :param pad: Int setting padding amount. Default is none
+        """
+
+        if pad:
+            wavefront.pad_wavefront(pad)
 
         wave_k = wavefront.omega / c_light
         lambd = 2.0 * torch.pi / wave_k
 
+        # Get new axes
         deltaX_i = (wavefront.wf_bounds[1] - wavefront.wf_bounds[0]) \
                    / wavefront.n_samples_xy[0]
         deltaY_i = (wavefront.wf_bounds[3] - wavefront.wf_bounds[2]) \
@@ -47,14 +150,17 @@ class FraunhoferProp(OpticalElement):
         y_new = torch.linspace(prop_bounds[2], prop_bounds[3],
                                wavefront.n_samples_xy[1],
                                device=wavefront.device)
-        x_new_mesh, y_new_mesh = torch.meshgrid(x_new, y_new, indexing="xy")
+        x_new_mesh, y_new_mesh = torch.meshgrid(x_new, y_new, indexing="ij")
+
+        # Calculate new field
         c = 1 / (1j * lambd * self.z) \
             * torch.exp(1j * wave_k / (2. * self.z) * (x_new_mesh**2. +
                                                        y_new_mesh**2.))
         field = wavefront.field.reshape(wavefront.n_samples_xy[0],
-                                         wavefront.n_samples_xy[1], 2)
+                                        wavefront.n_samples_xy[1], 2)
         new_field = c[:, :, None] * torch.fft.ifftshift(torch.fft.fft2(
             torch.fft.fftshift(field, dim=(0, 1)), dim=(0, 1)), dim=(0, 1))
+
         # Update bounds / field
         wavefront.field = new_field.flatten(0, 1)
         wavefront.z = wavefront.z + self.z
@@ -82,76 +188,41 @@ class CircularAperture(OpticalElement):
         mask = torch.where(r < self.radius, 1, 0)[:, None]
         print(r.get_device(), mask.device)
         wavefront.field = wavefront.field * mask
-class FresnelProp(OpticalElement):
-
-    def __init__(self, distance):
-        self.distance = distance
-
-    def propagate(self, wavefront):
-
-        """
-        freq_x = torch.fft.fftfreq(wavefront.n_samples_xy[0],
-                             d=(wavefront.wf_bounds[1] - wavefront.wf_bounds[0])
-                               / wavefront.n_samples_xy[0])
-        freq_y = torch.fft.fftfreq(wavefront.n_samples_xy[1],
-                             d=(wavefront.wf_bounds[3] - wavefront.wf_bounds[2])
-                               / wavefront.n_samples_xy[1])
-        freq_x, freq_y = torch.meshgrid(freq_x, freq_y, indexing="xy")
-        tf = torch.exp(1j * (wavefront.omega * self.distance / c_light -
-                             2. * torch.pi**2.0 * c_light * self.distance
-                             * (freq_x**2.0 + freq_y**2.0) / wavefront.omega))
-        field_k = torch.fft.fft2(,
-            dim=(0, 1))
-        wavefront.field = torch.fft.ifft2(field_k * tf[:, :, None],
-                                          dim=(0, 1)).flatten(0, 1)
-        """
 
 
-class RayleighSommerfeldProp(OpticalElement):
 
-    def __init__(self, distance):
-        self.distance = distance
 
-    def propagate(self, wavefront):
-        freq_x = torch.fft.fftfreq(wavefront.n_samples_xy[0],
-                             d=(wavefront.wf_bounds[1] - wavefront.wf_bounds[0])
-                               / wavefront.n_samples_xy[0])
-        freq_y = torch.fft.fftfreq(wavefront.n_samples_xy[1],
-                             d=(wavefront.wf_bounds[3] - wavefront.wf_bounds[2])
-                               / wavefront.n_samples_xy[1])
-        freq_x, freq_y = torch.meshgrid(freq_x, freq_y, indexing="xy")
-        tf = torch.exp(1j * (wavefront.omega * self.distance / c_light -
-                             2. * torch.pi**2.0 * c_light * self.distance
-                             * (freq_x**2.0 + freq_y**2.0) / wavefront.omega))
-        field_k = torch.fft.fft2(wavefront.field.reshape(
-            wavefront.n_samples_xy[0], wavefront.n_samples_xy[1], 2),
-            dim=(0, 1))
-        wavefront.field = torch.fft.ifft2(field_k * tf[:, :, None],
-                                          dim=(0, 1)).flatten(0, 1)
 
 
 if __name__ == "__main__":
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     track = Track(device=device)
     track.load_file("./track.npy")
-    wavefnt = Wavefront(1.7526625849289021, 3.77e6,
-                        np.array([-0.022, 0.022, -0.022, 0.022]),
-                        np.array([200, 200]), device=device)
-    slvr = EdgeRadSolver(wavefnt, track)
-    slvr.auto_res()
-    slvr.solve(200)
-    #wavefnt.field[:, :] = 1
-    aper = CircularAperture(0.02)
-    aper.propagate(wavefnt)
-    wavefnt.pad_wavefront(3)
+    wavefnt = Wavefront(0, 3.77e6,
+                        np.array([-0.051, 0.051, -0.051, 0.051]),
+                        np.array([100, 100]), device=device)
+
+    wavefnt.field[:, 1] = 1
+    wavefnt.pad_wavefront(5)
+    free_space_prop = FresnelProp(2000)
+    free_space_prop.propagate(wavefnt)
     wavefnt.plot_intensity()
 
+    wavefnt = Wavefront(0, 3.77e6,
+                        np.array([-0.0001, 0.0001, -0.0001, 0.0001]),
+                        np.array([100, 100]), device=device)
 
-    free_space_prop = FraunhoferProp(30)
+    wavefnt.field[:, 1] = 1
+    wavefnt.pad_wavefront(2)
+    free_space_prop = RayleighSommerfeldProp(0.1)
     free_space_prop.propagate(wavefnt)
+    wavefnt.plot_intensity()
 
+    """
 
     wavefnt.plot_intensity(ds_fact=32)#log_plot=False, axes_lim=[-8e-5, 8e-5,
     # -8e-5,
     # 8e-5])
+    
+    """
     plt.show()

source/Wavefront.py
@@ -22,14 +22,14 @@ class Wavefront(torch.nn.Module):
         self.n_samples_xy = n_samples_xy
         self.device = device
         self.n_samples = n_samples_xy[0] * n_samples_xy[1]
+        self.delta = [(wf_bounds[1] - wf_bounds[0]) / n_samples_xy[0],
+                      (wf_bounds[3] - wf_bounds[2]) / n_samples_xy[1]]
         self.x_axis = torch.linspace(wf_bounds[0], wf_bounds[1],
                                      n_samples_xy[0], device=device)
         self.y_axis = torch.linspace(wf_bounds[2], wf_bounds[3],
                                      n_samples_xy[1], device=device)
         self.x_array, self.y_array = torch.meshgrid(self.x_axis, self.y_axis,
-                                                    indexing="xy")
-
-        # Create coordinate array
+                                                    indexing="ij")
         self.coords = torch.stack((self.x_array, self.y_array,
                                    z * torch.ones_like(self.x_array)),
                                   dim=2).flatten(0, 1)
@@ -52,7 +52,7 @@ class Wavefront(torch.nn.Module):
         self.y_axis = torch.linspace(self.wf_bounds[2], self.wf_bounds[3],
                                      self.n_samples_xy[1], device=self.device)
         self.x_array, self.y_array = torch.meshgrid(self.x_axis, self.y_axis,
-                                                    indexing="xy")
+                                                    indexing="ij")
         self.coords = torch.stack((self.x_array, self.y_array,
                                    self.z * torch.ones_like(self.x_array)),
                                   dim=2).flatten(0, 1)
@@ -76,7 +76,7 @@ class Wavefront(torch.nn.Module):
         self.y_axis = torch.linspace(self.wf_bounds[2], self.wf_bounds[3],
                                      self.n_samples_xy[1], device=self.device)
         self.x_array, self.y_array = torch.meshgrid(self.x_axis, self.y_axis,
-                                                    indexing="xy")
+                                                    indexing="ij")
         self.coords = torch.stack((self.x_array, self.y_array,
                                    self.z * torch.ones_like(self.x_array)),
                                   dim=2).flatten(0, 1)
@@ -88,8 +88,8 @@ class Wavefront(torch.nn.Module):
         """
         intensity = (torch.abs(self.field[:, 0])**2.0
                      + torch.abs(self.field[:, 1])**2.0).cpu().detach().numpy()
-        intensity = intensity.reshape(self.n_samples_xy[1],
-                                      self.n_samples_xy[0])
+        intensity = intensity.reshape(self.n_samples_xy[0],
+                                      self.n_samples_xy[1]).T
         fig, ax = plt.subplots()
 
         if log_plot:
@@ -101,8 +101,7 @@ class Wavefront(torch.nn.Module):
         else:
             pcol = ax.pcolormesh(self.x_axis.cpu().detach().numpy()[::ds_fact],
                                  self.y_axis.cpu().detach().numpy()[::ds_fact],
-                                 intensity[::ds_fact, ::ds_fact]
-                                 / (np.max(intensity)), vmin=0, vmax=1,
+                                 intensity[::ds_fact, ::ds_fact],
                                  cmap="jet")
             fig.colorbar(pcol)
 