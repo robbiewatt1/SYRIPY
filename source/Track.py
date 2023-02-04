a18142a75e068c59c005a451ea5a41238a237345
robbiewatt1 <rwatt1@stanford.edu>
2023-02-03 16:52:29 -0800

    fixed bugs and now rk4 solver is working.

--- source/Track.py
+++ source/Track.py
@@ -3,6 +3,10 @@ import torch
 import torch.linalg
 import matplotlib.pyplot as plt
 
+me = 9.1093837e-31
+qe = 1.60217663e-19
+c_light = 0.29979245
+
 
 class Track(torch.nn.Module):
     """
@@ -38,15 +42,20 @@ class Track(torch.nn.Module):
         self.r = torch.tensor(r, device=self.device)
         self.beta = torch.tensor(beta, device=self.device)
 
-    def plot_track(self, axes):
+    def plot_track(self, axes, pos=True):
         """
         Plot interpolated track (Uses cubic spline)
         :param axes: Axes to plot (e. g z-x [2, 0])
+        :param pos: Bool if true then plot position else plot beta
         :return: fig, ax
         """
         fig, ax = plt.subplots()
-        ax.plot(self.r[:, axes[0]].cpu().detach().numpy(),
-                self.r[:, axes[1]].cpu().detach().numpy())
+        if pos:
+            ax.plot(self.r[:, axes[0]].cpu().detach().numpy(),
+                    self.r[:, axes[1]].cpu().detach().numpy())
+        else:
+            ax.plot(self.beta[:, axes[0]].cpu().detach().numpy(),
+                    self.beta[:, axes[1]].cpu().detach().numpy())
         return fig, ax
 
     def sim_single(self, field_container, time, r_0, beta_0):
@@ -67,49 +76,46 @@ class Track(torch.nn.Module):
         self.beta = torch.zeros((self.time.shape[0], 3), device=self.device)
         self.field_index = torch.zeros_like(time, dtype=torch.int,
                                             device=self.device)
-        r_0.requires_grad = True
-        beta_0.requires_grad = True
         self.r[0] = r_0
         self.beta[0] = beta_0
         self.field_index[0] = 0
 
         for i, t in enumerate(time[:-1]):
             self.field_index[i] = field_container.get_index(self.r[i, 2],
-                                                            self.field_index[i])
+                                                        self.field_index[i-1])
             field = field_container.get_field(self.r[i], self.field_index[i])
             r_k1 = self.beta[i]
             beta_k1 = self._db_dt(r_k1, field)
 
-            field = field_container.get_field(self.r[i] + r_k1 * delta_t_2,
-                                              self.field_index[i])
+            field = field_container.get_field(self.r[i] + c_light * r_k1
+                                              * delta_t_2, self.field_index[i])
             r_k2 = self.beta[i] + beta_k1 * delta_t_2
             beta_k2 = self._db_dt(r_k2, field)
 
-            field = field_container.get_field(self.r[i] + r_k2 * delta_t_2,
-                                              self.field_index[i])
+            field = field_container.get_field(self.r[i] + c_light * r_k2
+                                              * delta_t_2, self.field_index[i])
             r_k3 = self.beta[i] + beta_k2 * delta_t_2
             beta_k3 = self._db_dt(r_k3, field)
 
-            field = field_container.get_field(self.r[i] + r_k3 * detla_t,
-                                              self.field_index[i])
+            field = field_container.get_field(self.r[i] + c_light * r_k3
+                                              * detla_t, self.field_index[i])
             r_k4 = self.beta[i] + beta_k3 * detla_t
             beta_k4 = self._db_dt(r_k4, field)
-
-            self.r[i+1] = self.r[i] + (delta_t_2 / 3.) * (r_k1 + 2. * r_k2
-                                                          + 2. * r_k3 + r_k4)
-            self.beta[i+1] = self.beta[i] + (delta_t_2 / 3.) \
+            self.r[i+1] = self.r[i] + (detla_t * c_light / 6.) \
+                          * (r_k1 + 2. * r_k2 + 2. * r_k3 + r_k4)
+            self.beta[i+1] = self.beta[i] + (detla_t / 6.) \
                              * (beta_k1 + 2. * beta_k2 + 2. * beta_k3 + beta_k4)
 
     @staticmethod
     def _db_dt(beta, field):
         """
-        Rate of change of beta w.r.t time.
+        Rate of change of beta w.r.t time. We assume acceleration is always
+        perpendicular to velocity which is true for just magnetic field
         :param beta: Particle velocity
         :param field: Magnetic field
         :return: beta gradient
         """
-        return (1. - torch.sum(beta * beta))**1.5 \
-               * torch.cross(beta, field)
+        return -(1. - torch.sum(beta * beta))**0.5 * torch.cross(beta, field)
 
 
 class FieldBlock:
@@ -122,7 +128,7 @@ class FieldBlock:
         """
         :param center_pos: Central position of the magnet.
         :param length: Length of main part of field.
-        :param B0: Field parameter vector.
+        :param B0: Field parameter vector [Units are tesla and meters.]
         :param direction: Vector through central axis of magnet [0, 0, 1]
             by defult
         :param edge_length: Length for field to fall by 10 %. 0 for hard edge or
@@ -132,7 +138,7 @@ class FieldBlock:
         """
         self.center_pos = torch.tensor(center_pos)
         self.length = length
-        self.B0 = B0
+        self.B0 = B0 / (me / (qe * 1.e-9))
         self.direction = direction
         self.edge_scaled = edge_length / (10**0.5 - 1)**0.5
         self.field_extent = field_extent * edge_length
@@ -195,6 +201,7 @@ class Dipole(FieldBlock):
         :return: Magnetic field vector.
         """
         local_pos = position - self.center_pos
+        print(position, local_pos)
         zr = torch.abs(local_pos[2]) - 0.5 * self.length
         if zr < 0:
             return self.B0
@@ -288,23 +295,25 @@ class FieldContainer:
         if self.field_idx[index] == -1:
             return torch.tensor([0., 0., 0.])
         else:
+            print(self.field_idx[index])
             return self.field_array[self.field_idx[index]].get_field(position)
 
 
 if __name__ == "__main__":
-    torch.autograd.set_detect_anomaly(True)
-    q1 = Dipole([0, 0, 0], 1e6, 1e-5, None, 0.05, 5)
-    test = FieldContainer([q1])
+    q1 = Dipole([0, 0, 0], 1, 0.1, None, 0.05, 5)
+    q2 = Dipole([0, 0, 2], 1, -0.1, None, 0.05, 5)
+    test = FieldContainer([q1, q2])
+
+    gamma = 100
 
     track = Track()
-    beta0 = torch.tensor([0, 0, 0.9])
+    beta0 = torch.tensor([0., 0, 1.]) * (1 - 1./gamma**2.)**0.5
     r0 = torch.tensor([0., 0., -1.])
-    time = torch.linspace(0, 0.1, 100)
+    time = torch.linspace(0, 15, 100)
     track.sim_single(test, time, r0, beta0)
-    track.plot_track([2, 0])
-    track.beta[-1, 0].backward()
-    print(track.beta[-1, 0])
-    print(beta0.grad)
+    track.plot_track([2, 0], True)
+    print(track.r[:, 2])
+
 
     plt.show()
 
