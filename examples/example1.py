6cc0a2771ea7e6185a0e075a7fb111ddbfac53ef
Robbie Watt <rwatt1@stanford.edu>
2023-03-29 11:41:25 -0700

    Updated basic example

--- examples/example1.py
+++ examples/example1.py
@@ -1,53 +1,69 @@
-from ERS import Wavefront, FieldSolver
-from ERS.Optics import FraunhoferProp, CircularAperture
+from ERS import Wavefront, FieldSolver, BeamSolver
+from ERS.Optics import FraunhoferProp, CircularAperture, OpticsContainer
 from ERS.Tracking import Track, Dipole, FieldContainer
 import torch
 import matplotlib.pyplot as plt
 
-# Check for gpu
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# Define field
-d1 = Dipole([0, 0, 0], 0.203274830142196, -0.49051235, None, 0.01)
-d2 = Dipole([0, 0, 1.0334], 0.203274830142196, -0.49051235, None,
-            0.01)
-field = FieldContainer([d1, d2])
+# Define the magnet setup
+d0 = Dipole(torch.tensor([-0.1156, 0, 0]),      # Location (m)
+            0.203274830142196,                  # Length (m)
+            torch.tensor([0, 0.49051235, 0]),   # Field strength (T)
+            None,                               # Direction (not implimented yet)
+            0.05)                               # Edge length (m)
+d1 = Dipole(torch.tensor([0, 0, 1.0334]), 0.203274830142196,
+            torch.tensor([0, -0.49051235, 0]), None, 0.05)
+d2 = Dipole(torch.tensor([0, 0, 2.0668]), 0.203274830142196,
+            torch.tensor([0, -0.49051235, 0]), None, 0.05)
+field = FieldContainer([d0, d1, d2])
 
-# define electron properties
-gamma = 339.3 / 0.51099890221
-d0 = torch.tensor([0.09313368161783511, 0, 1])
-r0 = torch.tensor([-0.09311173301, 0, -1])
+# Define the particle track
+gamma = 339.3 / 0.51099890221              # Lorentz factor
+d0 = torch.tensor([-0e-3, 0e-3, 1])        # Initial direction
+r0 = torch.tensor([-0.1155863873, 0, -1])  # Initial position
+time = torch.linspace(0, 14, 10000)         # Time array samples (ns)
 
-# Solve the track
+# Define tracking class and track (using c++ implementation, faster but can't
+# do gradients)
 track = Track(device=device)
-time = torch.linspace(0, 10, 10000)
 track.sim_single_c(field, time, r0, d0, gamma)
 
-# Plot the track
-track.plot_track(axes=[2, 0])
+# Plot track
+fig, ax = track.plot_track([2, 0])
+ax.set_xlabel("z (m)")
+ax.set_ylabel("x (m)")
 
-# Define the wavefront and field solver
-wavefnt = Wavefront(1.7526625849289021, 3.77e5,
-                    [-0.01, 0.01, -0.01, 0.01],
-                    [1000, 1000], device=device)
-solver = FieldSolver(wavefnt, track, device=device)
+# Define the initial wavefront
+wavefnt = Wavefront(2.786062584928902,             # z position of the wavefront (m)
+                    3.77e5,                        # Radiation angular frequency (2 pi / ns)
+                    [-0.02, 0.02, -0.02, 0.02],    # Wavefront size [x_min, x_max, y_min, y_max] (m)
+                    [500, 500],                    # Samples in x and y [n_x, n_y]
+                    device=device)                 # device used
 
-# Reduce the time samples to 200
-solver.set_dt(200)
+# Define the optics
+aper = CircularAperture(0.02)  # Aperture with radiuse 0.02m
+prop = FraunhoferProp(1)       # Fraunhofer propagation to 1m
+optics = OpticsContainer([aper, prop])
+
+# Define the field solver class
+solver = FieldSolver(wavefnt, track)
+# Set samples along track
+solver.set_dt(250,       # Number of new samples
+              4,         # Start time
+              13)        # End time
+fig, ax = track.plot_track(axes=[2, 0])
+ax.set_xlabel("z (m)")
+ax.set_ylabel("x (m)")
 
 # Solve the field
-solver.solve()
+solver.solve_field()
 
 # Plot the intensity
 wavefnt.plot_intensity()
 
-# Propagate through an aperture and to focus
-aper = CircularAperture(0.01)
-aper.propagate(wavefnt)
-prop = FraunhoferProp(0.105)
-prop.propagate(wavefnt, new_shape=[2000, 2000],
-               new_bounds=[-0.0015, 0.0015, -0.0015, 0.0015])
-
-# Plot the intensity again
+# Propagate the wavefront
+optics.propagate(wavefnt)
 wavefnt.plot_intensity()
+
 plt.show()
