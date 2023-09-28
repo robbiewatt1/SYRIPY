ad7363fd9f0dbd9f273cb3ee004a9226ae20ddf7
Robbie Watt <rwatt1@stanford.edu>
2023-09-28 16:29:39 -0700

    Updated bayesian example

--- examples/Inference_examples/Bayesian.py
+++ examples/Inference_examples/Bayesian.py
@@ -1,6 +1,6 @@
-from ERS import Wavefront, FieldSolver
-from ERS.Optics import FraunhoferProp, CircularAperture, OpticsContainer
-from ERS.Tracking import Track, Dipole, FieldContainer
+from SYRIPY import Wavefront, FieldSolver
+from SYRIPY.Optics import FraunhoferProp, CircularAperture, OpticsContainer
+from SYRIPY.Tracking import Track, Dipole, FieldContainer
 import torch
 import numpy as np
 import pyro
@@ -8,8 +8,6 @@ import pyro.distributions as dist
 import seaborn
 import pandas
 import matplotlib.pyplot as plt
-
-
 
 # In this example we will show how the package can be used to perform
 # Bayesian inference on mock experimental data. For this we will use the
@@ -57,12 +55,13 @@ class ForwardModel:
         # Set the known / initial assumption beam parameters for the simulation
         self.time = torch.linspace(0, 18, 1001)
         self.d0 = torch.tensor([-0, 0, 1.])
-        self.r0 = torch.tensor([-0.1155863873, 0, -1.00])
-        self.g0 = 339.3 / 0.51099890221
+        self.r0 = torch.tensor([-0.091386, 0, -1.00])
+        self.g0 = torch.tensor([339.3 / 0.51099890221])
 
         # Define the track / wavefront  / solver classes for the simulation
-        self.track = Track(device=device)
-        self.wavefront = Wavefront(3.786062584928902, 3.77e5,
+        self.track = Track(self.field, device=device)
+        self.track = torch.jit.script(self.track)
+        self.wavefront = Wavefront(3.086, 3.77e5,
                                    [-0.02, 0.02, -0.02, 0.02],
                                    [300, 1], device=device)
         self.solver = FieldSolver(self.wavefront, self.track)
@@ -70,12 +69,11 @@ class ForwardModel:
         # Finally we wrap the solver function with torch.func.vmap, allowing the
         # calculation to be batched.
         def solver_func(index):
-            wvfrt = self.solver.solve_field(1, True, True, index)
-            self.optics.propagate(wvfrt)
+            wvfrt = self.solver.solve_field_vmap(index, True)
             return wvfrt.get_intensity()
         self.solver_func = torch.func.vmap(solver_func)
 
-    def forward(self, dir_x, div_x):
+    def forward(self, size_x, div_x):
         """
         This represents a forward call to the model. In this tutorial we are
         interested in inferring the direction and divergence of the beam in the
@@ -91,16 +89,18 @@ class ForwardModel:
         # involves sampling from a standard normal and shifting / rescaling by
         # dir_x and div_x respectively
         bunch_d = self.d0 + div_x * torch.tensor([1., 0., 0.]) * torch.randn(
-            self.samples)[:, None] + torch.tensor([1., 0., 0.]) * dir_x
-        bunch_r = self.r0.repeat(self.samples, 1)
+            self.samples)[:, None]
+        bunch_r = self.r0 + size_x * torch.tensor([1., 0., 0.]) * torch.randn(
+            self.samples)[:, None]
         bunch_g = torch.ones(self.samples) * self.g0
 
         # Now we simulate the tracks. Can't use the c++  version as gradients
         # are required
-        self.track.sim_bunch(self.field, bunch_r, bunch_d, bunch_g, self.time)
+        self.track.sim_beam(self.time, bunch_r=bunch_r, bunch_d=bunch_d,
+                            bunch_g=bunch_g)
 
         # Now we down-sample the track so the field solver is faster
-        self.solver.set_dt(201, t_start=4, t_end=13, set_bunch=True)
+        self.solver.set_track(201, t_start=4, t_end=13)
 
         # Finally we calculate the radiation intensity for the beam
         intensity_total = torch.zeros((300, 1), device=self.track.device)
@@ -108,7 +108,7 @@ class ForwardModel:
             batch_idx = torch.arange(batch * self.n_batch,
                                      (batch + 1) * self.n_batch)
             intensity_total += torch.mean(self.solver_func(batch_idx), dim=0)
-        return intensity_total.T / self.n_batch
+        return intensity_total.T / int(self.samples / self.n_batch)
 
 
 def pryo_model(forward_model, measured_intensity=None):
@@ -122,13 +122,13 @@ def pryo_model(forward_model, measured_intensity=None):
     :return: Sample of observable variable
     """
     # Start by sampling from the latent variables with sensible uniform priors
-    dir = pyro.sample('Dir', dist.Uniform(-3e-3, 3e-3))
+    size = pyro.sample('Size', dist.Uniform(0, 1e-3))
     div = pyro.sample('Div', dist.Uniform(0, 1e-3))
-    sigma = pyro.sample("Sigma", dist.Uniform(0.00, 0.3))
+    sigma = pyro.sample("Sigma", dist.Uniform(0.00, 0.003))
 
     # Now call the forward model wiht the sample
-    predict = forward_model.forward(dir, div).to("cpu")
-    predict = predict / torch.max(predict)
+    predict = forward_model.forward(size, div).to("cpu")
+    predict = predict / torch.trapz(predict)
 
     # Now sample the observed random variable
     with pyro.plate("data", predict.shape[1]):
@@ -145,61 +145,66 @@ if __name__ == "__main__":
 
     # Model a beam that is 1.2 mrad off axis with 300urad divergence and
     # normalise
-    intensity = forward_model.forward(1.2e-3, 0.30e-3)
-    intensity = intensity / torch.max(intensity)
+    intensity = forward_model.forward(0.6e-3, 0.1e-3)
+    intensity = intensity / torch.trapz(intensity)
 
     # Add some noise to the data
-    intensity = torch.normal(intensity, 0.05).to("cpu")
+    intensity = torch.normal(intensity, 0.0003).to("cpu")
 
     # Check what the mock data looks like
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(4, 3))
     ax.plot(intensity.cpu()[0], color="red")
     ax.set_xlabel("x [pixels]")
     ax.set_ylabel("I [a.u.]")
     ax.set_xlim([0, 300])
+    fig.tight_layout()
     plt.show()
 
     # We are going to be using variational inference. Therefore, we need to
     # define the variational distribution (guide), optimiser (adam) and loss
     # function (ELBO which is a lower bound of the KL divergence).
     auto_guide = pyro.infer.autoguide.AutoMultivariateNormal(pryo_model)
-    adam = pyro.optim.Adam({"lr": 0.05})
+    adam = pyro.optim.Adam({"lr": 0.04})
     elbo = pyro.infer.Trace_ELBO()
     svi = pyro.infer.SVI(pryo_model, auto_guide, adam, elbo)
 
     # We now run the optimiser for some number of steps
     losses = []
-    for step in range(50):
+    for step in range(100):
         loss = svi.step(forward_model, intensity)
         print(step, loss)
         losses.append(loss)
+    fig, ax = plt.subplots()
+    ax.plot(losses)
 
     # We can now sample from the guide distribution (i.e. approximation to the
     # posterior distribution)
-    with pyro.plate("samples", 1000, dim=-1):
+    with pyro.plate("samples", 10000, dim=-1):
         samples = auto_guide(forward_model)
 
     # Convert the samples to pandas for plotting
     samples = {k: v.detach().cpu().numpy() for k, v in samples.items()}
     data = pandas.DataFrame(samples)
+    data.to_csv("./samples.csv")
 
     # Plot the posterior marginals
     g = seaborn.pairplot(data, corner=True, diag_kind="kde")
-    g.map_upper(seaborn.scatterplot, s=15)
     g.map_lower(seaborn.kdeplot)
     g.map_diag(seaborn.kdeplot, lw=2)
 
     # Run the forward model with the mean of the posterior and compair it to the
     # mock data
-    dir_mean = np.mean(samples["Dir"])
+    dir_mean = np.mean(samples["Size"])
     div_mean = np.mean(samples["Div"])
     intensity2 = forward_model.forward(dir_mean, div_mean)
-    intensity2 = intensity2 / torch.max(intensity2)
-    fig, ax = plt.subplots()
+    intensity2 = intensity2 / torch.trapz(intensity2)
+
+    fig, ax = plt.subplots(figsize=(4, 3))
     ax.plot(intensity.cpu()[0], color="red", label="Measured")
     ax.plot(intensity2.cpu()[0], color="black", label="Predicted")
     ax.set_xlabel("x [pixels]")
     ax.set_ylabel("I [a.u.]")
     ax.set_xlim([0, 300])
-    ax.legend()
+    ax.legend(frameon=False)
+    fig.tight_layout()
     plt.show()
