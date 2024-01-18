54414eae3277989188446babd5e0930b879ebbe6
Robbie Watt <rwatt1@stanford.edu>
2024-01-18 14:25:55 -0800

    added better comments

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -5,6 +5,12 @@ from SYRIPY.Tracking import Track, Dipole, FieldContainer
 import torch
 import matplotlib.pyplot as plt
 
+# This example shows how SYRIPY can be used to simulate the production of
+# edge radiation in the centre of a bunch compressor. We first simulate a
+# particle track. Then we use this track to solve the field. Finally, we
+# propagate the field to the focal plane of a lens and plot the intensity.
+
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Define the magnet setup
@@ -31,7 +37,7 @@ track = Track(field, device=device)
 track.set_central_params(r0, p0)
 track.sim_central_c(time)
 
-# Plot track
+# Plot track (z-x plane)
 fig, ax = track.plot_track([2, 0])
 ax.set_xlabel("z (m)")
 ax.set_ylabel("x (m)")
@@ -40,20 +46,27 @@ ax.set_ylabel("x (m)")
 wavefnt = Wavefront(3.0,  # z position of the wavefront (m)
                     3.77e5,  # Radiation angular frequency (2 pi / ns)
                     [-0.02, 0.02, -0.02, 0.02],  # Wavefront size [x_min, x_max, y_min, y_max] (m)
-                    [500, 500],  # Samples in x and y [n_x, n_y]
+                    [250, 250],  # Samples in x and y [n_x, n_y]
                     device=device)  # device used
 
 # Define the optics
 aper = CircularAperture(0.02, pad=2)  # Aperture with radiuse 0.02m
-prop = FraunhoferPropQS(1)  # Fraunhofer propagation to 1m
+prop = FraunhoferPropQS(1.)  # Fraunhofer propagation to 1m
 optics = OpticsContainer([aper, prop])
 
 # Define the field solver class
 solver = FieldSolver(wavefnt, track)
-# Set samples along track
+
+# Select the track region of interest and redistribution the samples along the
+# track
 solver.set_track(201,  # Number of new samples
                  4,  # Start time
                  13,)  # End time
+
+# Plot the updated track
+fig, ax = track.plot_track(axes=[2, 0])
+ax.set_xlabel("z (m)")
+ax.set_ylabel("x (m)")
 
 # Solve the field
 wavefnt = solver.solve_field()
@@ -64,7 +77,7 @@ wavefnt.plot_intensity()
 # Propagate the wavefront
 optics.propagate(wavefnt)
 
-# Plot the intensity
+# Plot the final intensity
 wavefnt.plot_intensity()
 
 plt.show()
